ec2c525bf54f7c02010cd859a13f92b639d9e8e8
Ap0c <Ap0c@users.noreply.github.com>
2015-06-15 15:54:03 +0100

    Added insert snippet and heading functions to editing-tools.

--- js/editing-tools.js
+++ js/editing-tools.js
@@ -1,15 +1,52 @@
 // ----- Functions ----- //
 
-// Inserts a 
-function insert (document, snippet) {
+// Inserts a snippet into the editor.
+function insert (elements, snippet) {
+
+	var editor = elements.editor;
+	var selectStart = editor.selectionStart;
+	var selectEnd = editor.selectionEnd;
+
+	editor.value = editor.value.substring(0, selectStart) + snippet +
+		editor.value.substring(selectEnd);
+
+}
 
 
+// Inserts a heading of a given (number) size into the editor.
+function insertHeading (elements, size) {
+
+	var snippet = '';
+
+	switch (size) {
+		case 1:
+			snippet = '# ';
+			break;
+		case 2:
+			snippet = '## ';
+			break;
+		case 3:
+			snippet = '### ';
+			break;
+		case 4:
+			snippet = '#### ';
+			break;
+		case 5:
+			snippet = '##### ';
+			break;
+		case 6:
+			snippet = '###### ';
+	}
+
+	insert(elements, snippet);
 
 }
 
 
 // ----- Module Exports ----- //
 
-module.exports = function (document) {
-
+module.exports = function (elements) {
+	return {
+		insertHeading: insertHeading
+	};
 };
