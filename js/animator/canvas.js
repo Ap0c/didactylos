69960be9b97c605a441d9b01b6400ab82cd0bc3c
Ap0c <Ap0c@users.noreply.github.com>
2015-07-15 15:08:17 +0100

    Added ability to drag objects around on the canvas.

--- js/animator/canvas.js
+++ js/animator/canvas.js
@@ -6,6 +6,10 @@ module.exports = function Canvas (window) {
 	var canvas = document.getElementById('canvas');
 	var ctx = canvas.getContext('2d');
 	var drawings = [];
+	var dragging = {
+		current: false,
+		moveFunction: null
+	};
 
 	var properties = {
 		width: 500,
@@ -80,8 +84,21 @@ module.exports = function Canvas (window) {
 
 	}
 
+	// Retrieves the attributes of a drawing by id number.
+	function getDrawing (number) {
+
+		if (drawings[number]) {
+			return drawings[number].attrs;
+		} else {
+			throw new Error(`No drawing with id: ${number}`);
+		}
+
+	}
+
 	// Paints the drawings onto the canvas.
 	function paint () {
+
+		drawBackground();
 
 		ctx.fillStyle = properties.brush;
 
@@ -99,11 +116,83 @@ module.exports = function Canvas (window) {
 
 	}
 
+	// Gets the canvas co-ordinates of a mouse click.
+	function clickCoords (cursor) {
+
+		var canvasPos = canvas.getBoundingClientRect();
+
+		var x = (cursor.pageX - canvasPos.left) *
+			(canvas.width / canvasPos.width);
+		var y = (cursor.pageY - canvasPos.top) *
+			(canvas.height / canvasPos.height);
+
+		return { x: x, y: y };
+
+	}
+
+	// Returns function that updates the drawing position as it is dragged.
+	function drag (drawing) {
+
+		return function updateDrag (cursor) {
+
+			var coords = clickCoords(cursor);
+			drawing.attrs.x = coords.x;
+			drawing.attrs.y = coords.y;
+			drawing.changed = true;
+			paint();
+
+		};
+
+	}
+
+	// Checks if the user click was inside one of the drawings.
+	function inDrawing (coords) {
+
+		for (var i = drawings.length - 1; i >= 0; i--) {
+
+			if (ctx.isPointInPath(drawings[i].path, coords.x, coords.y)) {
+
+				dragging.current = true;
+				dragging.moveFunction = drag(drawings[i]);
+				canvas.addEventListener('mousemove', dragging.moveFunction);
+				return;
+
+			}
+
+		}
+
+	}
+
+	// Sets up the user's ability to drag drawings.
+	function setupDrag () {
+
+		canvas.addEventListener('mousedown', function (downEvent) {
+
+			console.log('mousedown');
+
+			var coords = clickCoords(downEvent);
+			inDrawing(coords);
+
+		});
+
+		canvas.addEventListener('mouseup', function cancelDrag (upEvent) {
+
+			if (dragging.current) {
+				dragging.current = false;
+				canvas.removeEventListener('mousemove', dragging.moveFunction);
+			}
+
+		});
+
+	}
+
 	// Initialises the canvas object.
 	function init () {
 
 		properties.width = canvas.width;
 		properties.height = canvas.height;
+
+		setupDrag();
 
 	}
 
