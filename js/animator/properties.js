c62e4a3399f2ab70359c97601a70c3920b4d1b21
Ap0c <Ap0c@users.noreply.github.com>
2015-07-25 17:09:54 +0100

    Filled out further methods on the properties sidebar.

--- js/animator/properties.js
+++ js/animator/properties.js
@@ -19,19 +19,88 @@ module.exports = function Properties (window) {
 
 	}
 
+	// Returns an input field type when given a data type.
+	function entryType (type) {
+
+		switch (type) {
+			case 'number':
+				return 'number';
+			case 'string':
+				return 'text';
+			case 'colour':
+				return 'color';
+			case 'boolean':
+				return 'checkbox';
+			default:
+				throw new Error(`Unrecognised data type: ${type}.`);
+		}		
+
+	}
+
+	// Sets the initial value of the field.
+	function entryValue (entry, value) {
+
+		if (type === 'number' || type === 'string' || type === 'colour') {
+			entry.value = value;
+		} else if (type === 'boolean') {
+			entry.checked = value;
+		}
+
+	}
+
 	// Adds a field to the list of fields to be appended.
-	function newField (name, type) {
+	function newField (name, type, value) {
 
 		var field = document.createElement('div');
 		var label = document.createElement('label');
 		var entry = document.createElement('input');
 
 		label.value = name;
+		entry.type = entryType(type);
+		entryValue(entry, value);
+
 		label.appendChild(entry);
 		field.appendChild(label);
 
 		return field;
 
 	}
+
+	// Adds the fields for a drawing to the properties sidebar.
+	function addFields (drawing) {
+
+		var fieldSet = document.createDocumentFragment();
+
+		for (var attr in drawing) {
+
+			if (Object.getOwnPropertyDescriptor(drawing, attr).set) {
+
+				var field = newField(attr, drawing.type(attr), drawing[attr]);
+				fieldSet.appendChild(field);
+
+			}
+
+		}
+
+		return fieldSet;
+
+	}
+
+	// Updates the properties sidebar to display the currently selected drawing.
+	function update (drawing) {
+
+		removeFields();
+
+		var newFields = addFields(drawing);
+		properties.appendChild(newFields);
+
+	}
+
+
+	// ----- Constructor ----- //
+
+	return {
+		update: update
+	};
 
 };
